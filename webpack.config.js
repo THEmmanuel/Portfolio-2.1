9ed78f1bf14a789c8b68e92d4f470571f40138ac
unknown <Emmanuel Ayodele>
2021-12-27 13:25:12 +0100

    Added style loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,21 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 }
-            }
-        }, ]
+            },
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
