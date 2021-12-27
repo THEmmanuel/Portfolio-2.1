c53bfe9f14d7fed729425f7b7201c2fd264c4d51
unknown <Emmanuel Ayodele>
2021-12-27 13:14:46 +0100

    Added css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, "src", "index.js"),
@@ -11,6 +12,9 @@ module.exports = {
             exclude: /node_modules/,
             use: {
                 loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
             }
         }, ]
     },
