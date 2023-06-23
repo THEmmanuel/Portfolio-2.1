f23a517a67b6e1a59b0f4775f59dff531ab3a8e9
Emmanuel <emmayodayo@gmail.com>
2023-06-23 13:28:06 +0100

    add media list

--- src/Data/MediaList.js
+++ src/Data/MediaList.js
@@ -1,19 +1,38 @@
 export const MediaList = [{
-		mediaName: 'Test',
-		mediaPublisher: 'Test stuff',
+		mediaName: 'Deploying Smart Contracts to the Polygon and Ethereum Blockchain Using Hardhat and Ethers.js',
+		mediaPublisher: 'Better Programming',
 		mediaDate: '2022',
-		mediaType: 'podcast'
+		mediaType: 'Article'
 	},
 	{
-		mediaName: 'Test',
-		mediaPublisher: 'Test stuff',
+		mediaName: 'Taking the First Step with Everyday Innovator Emmanuel Bello',
+		mediaPublisher: 'With Tamara Ghandour',
 		mediaDate: '2022',
-		mediaType: 'podcast'
+		mediaType: 'Podcast'
 	},
 	{
-		mediaName: 'Test',
-		mediaPublisher: 'Test stuff',
-		mediaDate: '2022',
-		mediaType: 'podcast'
+		mediaName: 'Working collaboratively with Git and GitHub',
+		mediaPublisher: 'Medium',
+		mediaDate: '2019',
+		mediaType: 'Article'
+	},
+	{
+		mediaName: 'Becoming World Class',
+		mediaPublisher: 'Medium',
+		mediaDate: '2019',
+		mediaType: 'Article'
+	},
+	{
+		mediaName: "Andela's Bootcamp, what I've learnt so far",
+		mediaPublisher: 'Medium',
+		mediaDate: '2019',
+		mediaType: 'Article'
+	},
+
+	{
+		mediaName: "My Reflections on Andela's EPIC values",
+		mediaPublisher: 'Medium',
+		mediaDate: '2019',
+		mediaType: 'Article'
 	},
 ]
