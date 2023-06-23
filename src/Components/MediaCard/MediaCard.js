0e5286c02b7692b2a3cadb6a75edd0c408cd0ca7
Emmanuel <emmayodayo@gmail.com>
2023-06-23 13:02:33 +0100

    add styling

--- src/Components/MediaCard/MediaCard.js
+++ src/Components/MediaCard/MediaCard.js
@@ -5,9 +5,9 @@ const MediaCard = () => {
 	return (
 		<div className={style.MediaCard}>
 			<div className={style.MediaCardWrapper}>
-				<span>Article Title</span>
-				<span>Article publisher</span>
-				<span>Article date</span>
+				<span className={style.MediaTitle}>Article Title</span>
+				<span className={style.MediaPublisher}>Article publisher</span>
+				<span className={style.MediaDate}>Article date | Type</span>
 			</div>
 		</div>
 	)
