8b2ae0ec3a442261420e31083413fa091b1c571f
Emmanuel <emmayodayo@gmail.com>
2023-06-24 05:45:03 +0100

    add certification links

--- src/Data/AchievementsList.js
+++ src/Data/AchievementsList.js
@@ -12,61 +12,74 @@ import calendar from '../Assets/brands/calendar.svg'
 
 
 export const CertificationsList = [{
-		brandName: "Software Architecture: Patterns for Developers (2023)",
-		brandLogo: linkedin
+		brandName: "Software Architecture: Patterns for Developers - 2023",
+		brandLogo: linkedin,
+		link: 'https://www.linkedin.com/learning/certificates/bb7944db075f9c4a6095d2d8509f1d873a9090251ee2fba1b6d3caa4c6a6c5b2'
 	},
 	{
-		brandName: "ETHOnline 2022 Staked Hacker",
-		brandLogo: ethglobal
+		brandName: "ETHOnline 2022 Staked Hacker - 2022",
+		brandLogo: ethglobal,
+		link: 'https://app.poap.xyz/token/5793811'
+
 	},
 	{
-		brandName: "Metabolism Staked Hacker",
-		brandLogo: ethglobal
+		brandName: "Metabolism Staked Hacker - 2022",
+		brandLogo: ethglobal,
+		link: 'https://app.poap.xyz/token/5518041'
 	},
 	{
-		brandName: "Data Structures and Algorithms",
-		brandLogo: udemy
+		brandName: "Data Structures and Algorithms - 2022",
+		brandLogo: udemy,
+		link: 'https://www.udemy.com/certificate/UC-a99f222e-c308-458a-8e0d-6d49abb28f3f/'
 	},
 	{
-		brandName: "HackMoney 2022 Staked Hacker",
-		brandLogo: ethglobal
+		brandName: "HackMoney 2022 Staked Hacker - 2022",
+		brandLogo: ethglobal,
+		link: 'https://app.poap.xyz/token/5120639'
 	},
 	{
-		brandName: "Build a Web3 App: Mint Your Own NFT collection - Buildspace NFT",
-		brandLogo: buildspace
+		brandName: "Build a Web3 App: Mint Your Own NFT collection - Buildspace NFT - 2022",
+		brandLogo: buildspace,
+		link: 'https://opensea.io/assets/matic/0x3CD266509D127d0Eac42f4474F57D0526804b44e/15041?force_update=true'
 	},
 	{
-		brandName: "Create a NFT game - Buildspace NFT",
-		brandLogo: buildspace
+		brandName: "Create a NFT game - Buildspace NFT - 2022",
+		brandLogo: buildspace,
+		link: 'https://opensea.io/assets/matic/0x3CD266509D127d0Eac42f4474F57D0526804b44e/15957?force_update=true'
 	},
 	{
-		brandName: "Building web applications with React",
-		brandLogo: pluralsight
+		brandName: "Building web applications with React - 2020",
+		brandLogo: pluralsight,
+		link: 'https://drive.google.com/file/d/122QpdgfxnUDPJh10XoQvStcLzQxHTbdm/view'
 	},
 	{
-		brandName: "HTML/CSS (Top 25% worldwide)",
-		brandLogo: testdome
+		brandName: "HTML/CSS (Top 25% worldwide) - 2020",
+		brandLogo: testdome,
+		link: 'https://www.testdome.com/certificates/30a37f7b023c44e5b2259901013c2187'
 	},
 	{
-		brandName: "HTML5",
-		brandLogo: pluralsight
+		brandName: "HTML5 - 2019",
+		brandLogo: pluralsight,
+		link: 'https://drive.google.com/file/d/126gKMi4CmascIo5Iup4-n6Q9jbs5uUwr/view'
 	},
 	{
-		brandName: "JavaScript",
-		brandLogo: pluralsight
+		brandName: "JavaScript - 2019",
+		brandLogo: pluralsight,
+		link: 'https://drive.google.com/file/d/12865bIqCpHUZQCvF6Nh23II0ML0RINhm/view'
 	},
 	{
-		brandName: "Pluralsight Associate Android Developer (AAD) IQ",
-		brandLogo: google
+		brandName: "Pluralsight Associate Android Developer (AAD) IQ - 2018",
+		brandLogo: google,
+		link: 'https://drive.google.com/file/d/0B5ZR-zhvXwv2MjZwRVlIbktkTEJZWmxrSUk1b29lVm8zZmhF/view?resourcekey=0-GNSD-xZr7fKOR3nX5brCBw'
 	},
 	{
-		brandName: "Android Basics Course",
-		brandLogo: google
+		brandName: "Google Africa Scholarships. Android Basics Course - 2018",
+		brandLogo: google,
+		link: 'https://drive.google.com/file/d/1Em1Yk51DMaxgeEkd2w4n4TE78SpADH-W/view'
 	},
 ]
 
-export const AchievementsList = [
-	{
+export const AchievementsList = [{
 		brandName: "170+ books read in three years",
 		brandLogo: books
 	},
