a9a71e0257cc79cda60d516ea886748c391d6f27
Emmanuel <emmayodayo@gmail.com>
2023-06-24 05:33:14 +0100

    add media links

--- src/Data/MediaList.js
+++ src/Data/MediaList.js
@@ -2,37 +2,44 @@ export const MediaList = [{
 		mediaName: 'Deploying Smart Contracts to the Polygon and Ethereum Blockchain Using Hardhat and Ethers.js',
 		mediaPublisher: 'Better Programming',
 		mediaDate: '2022',
-		mediaType: 'Article'
+		mediaType: 'Article',
+		link: 'https://betterprogramming.pub/deploying-smart-contracts-to-the-polygon-and-ethereum-blockchain-using-hardhat-and-ethers-js-2c31aa41aed0'
 	},
 	{
 		mediaName: 'Taking the First Step with Everyday Innovator Emmanuel Bello',
 		mediaPublisher: 'With Tamara Ghandour',
 		mediaDate: '2022',
-		mediaType: 'Podcast'
+		mediaType: 'Podcast',
+		link: 'https://www.gotolaunchstreet.com/podcast/taking-the-first-step-with-everyday-innovator-emmanuel-bello/'
 	},
 	{
 		mediaName: 'Working collaboratively with Git and GitHub',
 		mediaPublisher: 'Medium',
 		mediaDate: '2019',
-		mediaType: 'Article'
+		mediaType: 'Article',
+		link: 'https://medium.com/@emmanuelayodele/working-collaboratively-with-git-and-github-f5f4acdc1684?source=user_profile---------2----------------------------'
 	},
 	{
 		mediaName: 'Becoming World Class',
 		mediaPublisher: 'Medium',
 		mediaDate: '2019',
-		mediaType: 'Article'
+		mediaType: 'Article',
+		link: 'https://medium.com/@emmanuelayodele/becoming-world-class-7fa99b3d242f?source=user_profile---------4----------------------------'
 	},
 	{
 		mediaName: "Andela's Bootcamp, what I've learnt so far",
 		mediaPublisher: 'Medium',
 		mediaDate: '2019',
-		mediaType: 'Article'
+		mediaType: 'Article',
+		link: 'https://medium.com/@emmanuelayodele/andelas-bootcamp-what-i-ve-learnt-so-far-904a86d0a1b6?source=user_profile---------1----------------------------'
+
 	},
 
 	{
 		mediaName: "My Reflections on Andela's EPIC values",
 		mediaPublisher: 'Medium',
 		mediaDate: '2019',
-		mediaType: 'Article'
+		mediaType: 'Article',
+		link: 'https://medium.com/@emmanuelayodele/my-reflections-on-andelas-epic-values-6047ccb0e867?source=user_profile---------3----------------------------'
 	},
 ]
